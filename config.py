b4413589ffff58e07c2cba0e00e77e924070ca02
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-03 20:55:00 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+FINNHUB_TOKEN = os.getenv("FINNHUB_TOKEN")
+TWELVEDATA_API_KEY = os.getenv("TWELVEDATA_API_KEY")
+
+CHAT_ID = int(os.getenv("CHAT_ID"))
+THREAD_ID = int(os.getenv("THREAD_ID"))
+
+AUTHORIZED_USER_ID = int(os.getenv("AUTHORIZED_USER_ID", "0"))
+
+PAIR = "OANDA:XAU_USD"
